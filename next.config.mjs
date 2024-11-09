80e5ea63058b2e46ad8d9483fbe1be3061cb4d48
vladertec <sarnavsk2001@gmail.com>
2024-11-09 22:27:36 +0100

    update code for deploy  vercel-18

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,7 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-    },
-};
+  reactStrictMode: true,
+  // swcMinify: true,
+}
 
-export default nextConfig;
+export default nextConfig
